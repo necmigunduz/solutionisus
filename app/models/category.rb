4586ba3ed9311670def454fdfa16bf928f6bed6b
necmigunduz <necmigunduz@gmail.com>
2021-04-04 01:11:48 +0300

    Add associations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,4 @@
 class Category < ApplicationRecord
-  belongs_to :user_id
+  belongs_to :user
+  has_many :problems
 end
--- app/models/social_fact.rb
+++ app/models/social_fact.rb
@@ -1,2 +1,4 @@
 class SocialFact < ApplicationRecord
+    belongs_to :user
+    belongs_to :problem
 end
