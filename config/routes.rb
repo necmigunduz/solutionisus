91b98f357058be477afafaa5a00053a133089bfd
necmigunduz <necmigunduz@gmail.com>
2021-04-04 11:51:50 +0300

    Add root to the routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   resources :problems
   resources :categories
   resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root 'users#new'
 end
